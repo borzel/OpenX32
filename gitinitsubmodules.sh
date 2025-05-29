23703c0107f8534a1d179a6f228521d78b900c84
Christian Nöding <christian@noeding-online.de>
2025-05-29 12:05:05 +0200

    Fixed reduced checkout of submodules

--- gitinitsubmodules.sh
+++ gitinitsubmodules.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-echo "Preparing u-boot v2020.10..."
+echo "Preparing checkout of u-boot v2020.10..."
 cd u-boot
 git init
 git remote add origin https://github.com/u-boot/u-boot.git
-git fetch --depth 1 origin v2020.10
+git fetch --depth 1 origin tag v2020.10
 git checkout v2020.10
 cd ..
 
-echo "Preparing linux v6.12 LTS..."
+echo "Preparing checkout of linux v6.12 LTS..."
 cd linux
 git init
 git remote add origin https://github.com/torvalds/linux.git
-git fetch --depth 1 origin v6.12
+git fetch --depth 1 origin tag v6.12
 git checkout v6.12
 cd ..
 
-echo "pyATK will be cloned with full history..."
+echo "pyATK and busybox will be cloned with full history..."
 
-echo "Checking out..."
+echo "Checking out all submodules..."
 git submodule update --init --recursive
 
 echo "Done."
