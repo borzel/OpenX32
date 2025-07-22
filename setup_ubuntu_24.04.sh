7de1b3955048df16e13234ae2630d4149e1733fe
Alexander Schulz <code@schulzalex.de>
2025-07-22 18:21:17 +0200

    Setup on Ubuntu 24.04

---
+++ setup_ubuntu_24.04.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "This script will setup a Debian-based environment to allow compilation of the necessary components..."
+echo "Installing packages..."
+sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm fakeroot build-essential devscripts gcc-arm-none-eabi binutils-arm-none-eabi gcc-arm-linux-gnueabi binutils-arm-linux-gnueabi python3 python3-pip python3.12-venv u-boot-tools bc cpio
+
+echo "Configuring pyATK..."
+cp files/usbdev.py pyatk/pyatk/channel/
+cp files/boot.py pyatk/pyatk/
+cd pyatk
+python3 -m venv pyatk_venv
+source pyatk_venv/bin/activate
+pip install pyserial pyusb==1.0.0
+pip install setuptools
+sudo pyatk_venv/bin/python3 setup.py install
+deactivate
+cd ..
