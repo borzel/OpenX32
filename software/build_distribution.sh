8267079c522e434036cffa16637395ae08537381
Christian Nöding <christian@noeding-online.de>
2025-07-25 21:06:38 +0200

    Fixed compile-script for openssh

--- software/build_distribution.sh
+++ software/build_distribution.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Creating OpenX32 Distribution..."
 
-# create build-directory
+# create binary-directory
 mkdir bin
+mkdir bin/libexec
 
 echo "Compiling x32ctrl..."
 cd x32ctrl
@@ -22,8 +23,13 @@ cd ..
 echo "Compiling openssh-portable..."
 cd openssh-portable
 autoreconf
-CC=/usr/bin/arm-linux-gnueabi-gcc ./configure --without-openssl 
+CC=/usr/bin/arm-linux-gnueabi-gcc ./configure --without-openssl --host=armel
 make
+mv ssh-keygen ../bin/
+mv sshd_config ../bin/
+mv sshd ../bin/
+mv sshd-auth ../bin/libexec/
+mv sshd-session ../bin/libexec/
 cd ..
 
 echo "Download and copy all files to distribution-folder..."
