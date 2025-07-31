b3e761a9ae77f3b19511c5c7660cdedd4931fa6e
Christian Nöding <christian@noeding-online.de>
2025-07-31 23:57:04 +0200

    Cleaned-up libraries in initramfs and updated start-script for SSH to use initramfs

--- software/tests/gpiotest/compile.sh
+++ software/tests/gpiotest/compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/bin/arm-linux-gnueabi-gcc gpiotest.c -lgpiod -static -o ../../gpiotest
+#/usr/bin/arm-linux-gnueabi-gcc gpiotest.c -lgpiod -static -o gpiotest
+/usr/bin/arm-linux-gnueabi-gcc gpiotest.c -lgpiod -o gpiotest
